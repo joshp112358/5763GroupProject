f8da722c85a72f59543cf656ed02566ab1b9b741
Joshua Paik <joshuapaik@8afbeb0e.st-andrews.ac.uk>
2019-10-15 16:28:15 +0100

    added baseline

--- code/lmBoot.r
+++ code/lmBoot.r
@@ -1,27 +1,29 @@
 lmBoot <- function(inputData, nBoot){
   
+  
+  #argument 
+  
   for(i in 1:nBoot){
-    
     # resample our data with replacement
     bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
     
     # fit the model under this alternative reality
-    bootLM <- lm(y ~ x, data = bootData)
+    bootLM <- lm(Age ~ Weight, data = bootData)
     
     # store the coefs
     if(i == 1){
-      
       bootResults <- matrix(coef(bootLM), ncol = 2)
-
       } else {
-      
       bootResults<- rbind(bootResults, matrix(coef(bootLM), ncol = 2))
-
     }
     
-
   } # end of i loop
-  
   bootResults
-  
+}
+
+lmBoot(fitness,14)
+
+
+ggg <- function(n){
+   return(n*n)
 }
