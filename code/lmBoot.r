3bd12d8b95508095c273cc5ec8386967e3d8d155
Joshua Paik <joshuapaik@8afbeb0e.st-andrews.ac.uk>
2019-10-15 16:32:34 +0100

    reupload original lmboot code

--- code/lmBoot.r
+++ code/lmBoot.r
@@ -1,29 +1,27 @@
 lmBoot <- function(inputData, nBoot){
   
-  
-  #argument 
-  
   for(i in 1:nBoot){
+    
     # resample our data with replacement
     bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
     
     # fit the model under this alternative reality
-    bootLM <- lm(Age ~ Weight, data = bootData)
+    bootLM <- lm(y ~ x, data = bootData)
     
     # store the coefs
     if(i == 1){
+      
       bootResults <- matrix(coef(bootLM), ncol = 2)
+
       } else {
+      
       bootResults<- rbind(bootResults, matrix(coef(bootLM), ncol = 2))
+
     }
     
+
   } # end of i loop
+  
   bootResults
-}
-
-lmBoot(fitness,14)
-
-
-ggg <- function(n){
-   return(n*n)
+  
 }
