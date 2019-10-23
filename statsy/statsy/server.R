f216e7cf3a64141cf9edd82fd05d0401e3308712
X X <xx@xs-macbook-pro.home>
2019-10-23 20:21:17 +0100

    cleaned shiny

--- statsy/statsy/server.R
+++ statsy/statsy/server.R
@@ -1,71 +1,54 @@
 library(shiny)
 
 server <- function(input, output) {
+  speedyBoot <- function(inputData, num_var,formula, nBoots){
+    mat <- matrix(0L, nrow = nBoots, ncol = num_var)
+    for(i in 1:nBoots){
+      bootData <- inputData[sample(nrow(inputData), nrow(inputData), replace = T),]
+      bootLM <- lm(formula, data = bootData)
+      # store the coefs
+      mat[i,] <- coef(bootLM)
+    } # end of i loop
+    return(mat)
+  }
+  
+  coefficients2 <-reactive({
+        inFile <- input$data
+        data <- read.csv(inFile$datapath)
+        number_of_variables <- input$num_of_var
+        form <- input$formula
+        n <- input$num_of_boots
+        set.seed(9)
+        speedyBoot(inputData = data,
+                             num_var = number_of_variables,
+                             formula = form,
+                             nBoots = n)})
+  
+  
+  
   output$mean <- renderText(
-    {inFile <- input$data
-    data <- read.csv(inFile$datapath)
-    number_of_variables <- input$num_of_var
-    form <- input$formula
-    n <- input$num_of_boots
-    set.seed(9)
-    
-    speedyBoot <- function(inputData, num_var,formula, nBoots){
-      mat <- matrix(0L, nrow = nBoots, ncol = num_var)
-      for(i in 1:nBoots){
-        bootData <- inputData[sample(nrow(inputData), nrow(inputData), replace = T),]
-        bootLM <- lm(formula, data = bootData)
-        # store the coefs
-        mat[i,] <- coef(bootLM)
-      } # end of i loop
-      return(mat)
-    }
-    
-    coefficients2 <-speedyBoot(inputData = data,
-                               num_var = number_of_variables,
-                               formula = form,
-                               nBoots = n)
-    mean1<-mean(coefficients2[,1])
-    mean2<-mean(coefficients2[,2])
-    print(paste("####Mean intercept:", mean1,",Mean coeff1:", mean2));
-
+    {
+      number_of_variables <- input$num_of_var
+      holder = numeric(number_of_variables)
+      for(i in 1:number_of_variables){
+        holder[i]<-mean(coefficients2()[,i])
+      }
+    print(holder);
     })
   
   output$table <- renderTable({
-    inFile <- input$data
-    data <- read.csv(inFile$datapath)
     number_of_variables <- input$num_of_var
-    form <- input$formula
-    n <- input$num_of_boots
-    set.seed(9)
-    
-    speedyBoot <- function(inputData, num_var,formula, nBoots){
-      mat <- matrix(0L, nrow = nBoots, ncol = num_var)
-      for(i in 1:nBoots){
-        bootData <- inputData[sample(nrow(inputData), nrow(inputData), replace = T),]
-        bootLM <- lm(formula, data = bootData)
-        # store the coefs
-        mat[i,] <- coef(bootLM)
-      } # end of i loop
-      return(mat)
-    }
-    
-    coefficients2 <-speedyBoot(inputData = data,
-                               num_var = number_of_variables,
-                               formula = form,
-                               nBoots = n)
-      
       get_quantiles <- function(coeff, num_var){
         mat <- matrix(0L, nrow = 2, ncol = num_var)
         for(i in 1:num_var){
           mat[,i]<-matrix(quantile(coeff[,i], probs = c(0.025,0.975)))
         }
         #print(mat)
-        colnames(mat)=c("Intercept","Coeff1")
         rownames(mat)=c("2.5% Quartile","97.5% Quartiles")
         return(mat)
       }
       
-      get_quantiles(coefficients2,number_of_variables)
+      get_quantiles(coefficients2(),number_of_variables)
 
     },
     rownames = TRUE
